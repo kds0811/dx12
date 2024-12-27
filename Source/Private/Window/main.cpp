de0a5802313bcccd41373dc6e287e7c6db4c75b0
KolDS0811 <fatbahad@gmail.com>
2024-12-27 20:56:33 +0300

    add dx headers to project

--- Source/Private/Window/main.cpp
+++ Source/Private/Window/main.cpp
@@ -1,4 +1,12 @@
 #include "Window/WindowDK.h"
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <directx/d3dx12.h>
+
+
+
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
