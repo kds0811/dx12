f776343e6c73ed38935d4325b5f865cb2505ce92
KolDS0811 <fatbahad@gmail.com>
2025-01-11 15:05:01 +0300

    add base Vector3

---
+++ Source/Private/Utils/VectorDK.cpp
@@ -0,0 +1,12 @@
+#include "VectorDK.h"
+
+Vector3::Vector3()   
+{
+    Data.x = 0.0f;
+    Data.y = 0.0f;
+    Data.z = 0.0f;
+}
+
+Vector3::Vector3(float x, float y, float z) {}
+
+Vector3::Vector3(DirectX::XMFLOAT3 Vector) {}
