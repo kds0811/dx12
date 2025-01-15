db7cefff70b0449e60d095a55eaceda382289743
KolDS0811 <fatbahad@gmail.com>
2025-01-15 22:22:12 +0300

    add  camera class

--- Source/Public/App/Camera.h
+++ Source/Public/App/Camera.h
@@ -1,26 +1,17 @@
 #pragma once
 #include <DirectXMath.h>
+#include "Transform.h"
 
-class Camera
+class alignas(16) Camera
 {
+    Transform Transformation;
+
 public:
-    Camera();
+    inline Camera() noexcept : Transformation(Vector{0.f, 0.f, -10.f}, Rotator::Zero(), Vector(1.f, 1.f, 1.f)) {}
 
-    inline DirectX::XMFLOAT3 GetLocation() const noexcept { return Location; }
-    inline DirectX::XMFLOAT3 GetRotation() const noexcept { return Rotation; }
+    void MoveRight(float dt);
+    void MoveForward(float dt);
 
-    inline DirectX::XMFLOAT4 GetForwardVector() const noexcept { return ForwardVector; }
-    inline DirectX::XMFLOAT4 GetRightVector() const noexcept { return RightVector; }
-    inline DirectX::XMFLOAT4 GetUpVector() const noexcept { return UpVector; }
 
-private:
-    static constexpr DirectX::XMFLOAT3 StartLocation = {0.0f, -10.f, 0.0f};
-    static constexpr DirectX::XMFLOAT3 StartRotation = {0.0f, 0.0f, 0.0f};
 
-    DirectX::XMFLOAT3 Location;
-    DirectX::XMFLOAT3 Rotation;
-
-    DirectX::XMFLOAT4 ForwardVector;
-    DirectX::XMFLOAT4 RightVector;
-    DirectX::XMFLOAT4 UpVector;
 };
