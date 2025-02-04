ab19b6361bc055d498a896769659baa2af1f8079
KolDS0811 <fatbahad@gmail.com>
2025-02-04 21:14:26 +0300

    fix dds texture loader

--- Source/Public/Utils/DDSTextureLoader.h
+++ Source/Public/Utils/DDSTextureLoader.h
@@ -24,7 +24,10 @@
 
 #include <wrl.h>
 #include <d3d11_1.h>
-#include "d3dx12.h"
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <directx/d3dx12.h>
 
 #pragma warning(push)
 #pragma warning(disable : 4005)
