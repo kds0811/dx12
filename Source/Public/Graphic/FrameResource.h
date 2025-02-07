bf51edfe2beafbc562224d61fe50c95d0925725f
KolDS0811 <fatbahad@gmail.com>
2025-02-07 11:46:03 +0300

    add imgui files

--- Source/Public/Graphic/FrameResource.h
+++ Source/Public/Graphic/FrameResource.h
@@ -30,8 +30,8 @@ struct PassConstants
     DirectX::XMFLOAT4 AmbientLight = {0.0f, 0.0f, 0.0f, 1.0f};
 
     DirectX::XMFLOAT4 FogColor = {0.7f, 0.7f, 0.7f, 1.0f};
-    float gFogStart = 5.0f;
-    float gFogRange = 150.0f;
+    float gFogStart = 10.0f;
+    float gFogRange = 200.0f;
     DirectX::XMFLOAT2 cbPerObjectPad2;
 
     // Indices [0, NUM_DIR_LIGHTS) are directional lights;
