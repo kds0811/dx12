294076429201d3f8083ddae3e775ad7648925203
KolDS0811 <fatbahad@gmail.com>
2025-02-11 20:07:39 +0300

    delete tinyobjloader

--- Source/Public/Utils/TinyObjLoaderWrapper.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GeometryGenerator.h"
-#include <string>
-
-class TinyObjLoaderWrapper
-{
-public:
-    GeometryGenerator::MeshData LoadGeometryFromFile(std::string filePath);
-};
