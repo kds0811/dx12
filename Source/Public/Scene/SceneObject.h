fb877c48086d647f69e6cf277196643144557094
KolDS0811 <fatbahad@gmail.com>
2025-03-14 21:55:43 +0300

    add new scene base class SceneObject

---
+++ Source/Public/Scene/SceneObject.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <cstdint>
+#include <string>
+
+class SceneObject
+{
+public:
+    SceneObject() = default;
+    virtual ~SceneObject() = default;
+
+    inline void SetName(std::string name) { mName = name; }
+    inline std::string GetName() { return mName; }
+    inline void SetId(int id) { mID = id; }
+    inline std::uint64_t GetID() { return mID; }
+
+protected:
+    std::uint64_t mID;
+    std::string mName;
+};
