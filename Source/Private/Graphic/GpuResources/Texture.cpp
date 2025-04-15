d0ebc568f6e30d5e5966bd0327fa5d1e61112078
KolDS0811 <fatbahad@gmail.com>
2025-04-15 11:27:29 +0300

    Texture Creating changes

--- Source/Private/Graphic/GpuResources/Texture.cpp
+++ Source/Private/Graphic/GpuResources/Texture.cpp
@@ -2,25 +2,49 @@
 #include "GraphicError.h"
 #include "DDSTextureLoader.h"
 #include "Logger.h"
+#include "Device.h"
+#include "CommandList.h"
+#include "DescriptorHeapManager.h"
 
-Texture::Texture(ETextureType type, std::wstring name, std::wstring fileName, ID3D12GraphicsCommandList* cmdList)
+Texture::Texture(ETextureType type, const std::wstring& name, std::wstring fileName, CommandList* cmdList)
 	:
 	mType(type),
 	mFilename(std::move(fileName))
 {
-    LoadFromFile(device, cmdList);
+    LoadFromFile(name, cmdList);
 }
 
-void Texture::LoadFromFile(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList) 
+void Texture::LoadFromFile(const std::wstring& name, CommandList* cmdList)
 {
-    DirectX::CreateDDSTextureFromFile12(device, cmdList, mFilename.c_str(), GetComPtr(), mUploadHeap) >> Kds::App::Check;
-
+    Microsoft::WRL::ComPtr<ID3D12Resource> tempRes;
+    DirectX::CreateDDSTextureFromFile12(Device::GetDevice(), cmdList->GetCommandList(), mFilename.c_str(), tempRes, mUploadHeap) >> Kds::App::Check;
    
-	auto desc = mResource->GetDesc();
+	auto desc = tempRes->GetDesc();
     mFormat = desc.Format;
     mWidth = desc.Width;
     mHeight = desc.Height;
- 
+
     
-    LOG_MESSAGE("Texture ", GetName(), " is created");
+    SetResource(name, tempRes.Get(), D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+    BuildDescriptors();
+}
+
+
+void Texture::BuildDescriptors()
+{
+    mSrvDescriptorHandle = DescriptorHeapManager::AllocateCbvSrvUav();
+    if (mSrvDescriptorHandle.IsNull())
+    {
+        LOG_ERROR("Failed to allocate SRV descriptor.");
+        return;
+    }
+
+    D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+    srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+    srvDesc.Format = mFormat;
+    srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+    srvDesc.Texture2D.MostDetailedMip = 0;
+    srvDesc.Texture2D.MipLevels = 1;
+
+    Device::GetDevice()->CreateShaderResourceView(GetResource(), &srvDesc, mSrvDescriptorHandle.GetCpuHandle());
 }
--- Source/Public/Graphic/GpuResources/Texture.h
+++ Source/Public/Graphic/GpuResources/Texture.h
@@ -1,12 +1,15 @@
 #pragma once
 #include "GpuResource.h"
 
+class CommandList;
+
 enum class ETextureType : std::uint8_t
 {
     BaseColor,
     NormalMap,
     RoughnessMap,
     MetallicMap,
+    CubeMap,
     Default
 };
 
@@ -18,12 +21,16 @@ class Texture : public GpuResource
     DXGI_FORMAT mFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
     Microsoft::WRL::ComPtr<ID3D12Resource> mUploadHeap = nullptr;
     ETextureType mType = ETextureType::Default;
+    DescriptorHandle mSrvDescriptorHandle{};
+    D3D12_SRV_DIMENSION mSrvDimesion{};
 
 public:
-    Texture(ETextureType type, std::wstring name, std::wstring fileName, ID3D12GraphicsCommandList* cmdList);
+    Texture(ETextureType type, const std::wstring& name, std::wstring fileName, CommandList* cmdList);
 
     inline ETextureType GetTextureType() noexcept { return mType; }
 
 private:
-    void LoadFromFile(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList);
+    void LoadFromFile(const std::wstring& name, CommandList* cmdList);
+
+    void BuildDescriptors();
 };
