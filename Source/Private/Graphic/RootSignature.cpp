7a09cdf8a1ddc10ef023b6e42bea968df54a51aa
KolDS0811 <fatbahad@gmail.com>
2025-05-02 10:04:00 +0300

    code formating on Roo signature class

--- Source/Private/Graphic/RootSignature.cpp
+++ Source/Private/Graphic/RootSignature.cpp
@@ -1,6 +1,112 @@
 #include "RootSignature.h"
 #include "D3D12Utils.h"
 
+RootParameter::RootParameter()
+{
+    mRootParam.ParameterType = (D3D12_ROOT_PARAMETER_TYPE)0xFFFFFFFF;
+}
+
+RootParameter::~RootParameter()
+{
+    Clear();
+}
+
+void RootParameter::Clear() noexcept
+{
+    if (mRootParam.ParameterType == D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE)
+    {
+        delete[] mRootParam.DescriptorTable.pDescriptorRanges;
+    }
+    mRootParam.ParameterType = (D3D12_ROOT_PARAMETER_TYPE)0xFFFFFFFF;
+}
+
+void RootParameter::InitAsConstants(UINT Register, UINT NumDwords, D3D12_SHADER_VISIBILITY Visibility, UINT Space) noexcept
+{
+    mRootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS;
+    mRootParam.ShaderVisibility = Visibility;
+    mRootParam.Constants.Num32BitValues = NumDwords;
+    mRootParam.Constants.ShaderRegister = Register;
+    mRootParam.Constants.RegisterSpace = Space;
+}
+
+void RootParameter::InitAsConstantBuffer(UINT Register, D3D12_SHADER_VISIBILITY Visibility, UINT Space) noexcept
+{
+    mRootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+    mRootParam.ShaderVisibility = Visibility;
+    mRootParam.Descriptor.ShaderRegister = Register;
+    mRootParam.Descriptor.RegisterSpace = Space;
+}
+
+void RootParameter::InitAsBufferSRV(UINT Register, D3D12_SHADER_VISIBILITY Visibility, UINT Space) noexcept
+{
+    mRootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_SRV;
+    mRootParam.ShaderVisibility = Visibility;
+    mRootParam.Descriptor.ShaderRegister = Register;
+    mRootParam.Descriptor.RegisterSpace = Space;
+}
+
+void RootParameter::InitAsBufferUAV(UINT Register, D3D12_SHADER_VISIBILITY Visibility, UINT Space) noexcept
+{
+    mRootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_UAV;
+    mRootParam.ShaderVisibility = Visibility;
+    mRootParam.Descriptor.ShaderRegister = Register;
+    mRootParam.Descriptor.RegisterSpace = Space;
+}
+
+void RootParameter::InitAsDescriptorRange(D3D12_DESCRIPTOR_RANGE_TYPE Type, UINT Register, UINT Count, D3D12_SHADER_VISIBILITY Visibility, UINT Space) noexcept
+{
+    InitAsDescriptorTable(1, Visibility);
+    SetTableRange(0, Type, Register, Count, Space);
+}
+
+void RootParameter::InitAsDescriptorTable(UINT RangeCount, D3D12_SHADER_VISIBILITY Visibility)
+{
+    mRootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+    mRootParam.ShaderVisibility = Visibility;
+    mRootParam.DescriptorTable.NumDescriptorRanges = RangeCount;
+    mRootParam.DescriptorTable.pDescriptorRanges = new D3D12_DESCRIPTOR_RANGE[RangeCount];
+}
+
+void RootParameter::SetTableRange(UINT RangeIndex, D3D12_DESCRIPTOR_RANGE_TYPE Type, UINT Register, UINT Count, UINT Space)
+{
+    D3D12_DESCRIPTOR_RANGE* range = const_cast<D3D12_DESCRIPTOR_RANGE*>(mRootParam.DescriptorTable.pDescriptorRanges + RangeIndex);
+    range->RangeType = Type;
+    range->NumDescriptors = Count;
+    range->BaseShaderRegister = Register;
+    range->RegisterSpace = Space;
+    range->OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+}
+
+
+
+RootSignature::RootSignature(const std::wstring& name, UINT size) : mName(name)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        mParams.emplace_back();
+    }
+}
+
+RootSignature::~RootSignature() {}
+
+RootParameter* RootSignature::GetParam(size_t entryIndex)
+{
+    if (IsIndexValid(entryIndex))
+    {
+        return &mParams[entryIndex];
+    }
+    return nullptr;
+}
+
+const RootParameter* RootSignature::GetParam(size_t entryIndex) const
+{
+    if (IsIndexValid(entryIndex))
+    {
+        return &mParams[entryIndex];
+    }
+    return nullptr;
+}
+
 void RootSignature::Finalize(D3D12_ROOT_SIGNATURE_FLAGS Flags)
 {
     if (bFinalized) return;
@@ -24,3 +130,15 @@ void RootSignature::Finalize(D3D12_ROOT_SIGNATURE_FLAGS Flags)
 
     bFinalized = true;
 }
+
+bool RootSignature::IsIndexValid(size_t entryIndex) const noexcept
+{
+    assert(entryIndex < mParams.size());
+    if (entryIndex >= mParams.size())
+    {
+        LOG_ERROR("Index is out of range");
+        return false;
+    }
+    return true;
+}
+
--- Source/Public/Graphic/RootSignature.h
+++ Source/Public/Graphic/RootSignature.h
@@ -9,81 +9,21 @@ class RootParameter
     D3D12_ROOT_PARAMETER mRootParam;
 
 public:
-    RootParameter() { mRootParam.ParameterType = (D3D12_ROOT_PARAMETER_TYPE)0xFFFFFFFF; }
+    RootParameter();
+    ~RootParameter();
 
-    ~RootParameter() { Clear(); }
-
-    inline void Clear() noexcept
-    {
-        if (mRootParam.ParameterType == D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE)
-        {
-            delete[] mRootParam.DescriptorTable.pDescriptorRanges;
-        }
-        mRootParam.ParameterType = (D3D12_ROOT_PARAMETER_TYPE)0xFFFFFFFF;
-    }
-
-    inline void InitAsConstants(UINT Register, UINT NumDwords, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL, UINT Space = 0) noexcept
-    {
-
-        mRootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS;
-        mRootParam.ShaderVisibility = Visibility;
-        mRootParam.Constants.Num32BitValues = NumDwords;
-        mRootParam.Constants.ShaderRegister = Register;
-        mRootParam.Constants.RegisterSpace = Space;
-    }
-
-    inline void InitAsConstantBuffer(UINT Register, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL, UINT Space = 0) noexcept
-    {
-        mRootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
-        mRootParam.ShaderVisibility = Visibility;
-        mRootParam.Descriptor.ShaderRegister = Register;
-        mRootParam.Descriptor.RegisterSpace = Space;
-    }
-
-    inline void InitAsBufferSRV(UINT Register, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL, UINT Space = 0) noexcept
-    {
-        mRootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_SRV;
-        mRootParam.ShaderVisibility = Visibility;
-        mRootParam.Descriptor.ShaderRegister = Register;
-        mRootParam.Descriptor.RegisterSpace = Space;
-    }
-
-    inline void InitAsBufferUAV(UINT Register, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL, UINT Space = 0) noexcept
-    {
-        mRootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_UAV;
-        mRootParam.ShaderVisibility = Visibility;
-        mRootParam.Descriptor.ShaderRegister = Register;
-        mRootParam.Descriptor.RegisterSpace = Space;
-    }
-
-    inline void InitAsDescriptorRange(
-        D3D12_DESCRIPTOR_RANGE_TYPE Type, UINT Register, UINT Count, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL, UINT Space = 0) noexcept
-    {
-        InitAsDescriptorTable(1, Visibility);
-        SetTableRange(0, Type, Register, Count, Space);
-    }
-
+    void Clear() noexcept;
+    void InitAsConstants(UINT Register, UINT NumDwords, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL, UINT Space = 0) noexcept;
+    void InitAsConstantBuffer(UINT Register, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL, UINT Space = 0) noexcept;
+    void InitAsBufferSRV(UINT Register, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL, UINT Space = 0) noexcept;
+    void InitAsBufferUAV(UINT Register, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL, UINT Space = 0) noexcept;
+    void InitAsDescriptorRange(
+        D3D12_DESCRIPTOR_RANGE_TYPE Type, UINT Register, UINT Count, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL, UINT Space = 0) noexcept;
     [[nodiscard]] inline const D3D12_ROOT_PARAMETER& Get() const noexcept { return mRootParam; }
 
-
 private:
-    inline void InitAsDescriptorTable(UINT RangeCount, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL)
-    {
-        mRootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
-        mRootParam.ShaderVisibility = Visibility;
-        mRootParam.DescriptorTable.NumDescriptorRanges = RangeCount;
-        mRootParam.DescriptorTable.pDescriptorRanges = new D3D12_DESCRIPTOR_RANGE[RangeCount];
-    }
-
-    inline void SetTableRange(UINT RangeIndex, D3D12_DESCRIPTOR_RANGE_TYPE Type, UINT Register, UINT Count, UINT Space = 0)
-    {
-        D3D12_DESCRIPTOR_RANGE* range = const_cast<D3D12_DESCRIPTOR_RANGE*>(mRootParam.DescriptorTable.pDescriptorRanges + RangeIndex);
-        range->RangeType = Type;
-        range->NumDescriptors = Count;
-        range->BaseShaderRegister = Register;
-        range->RegisterSpace = Space;
-        range->OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
-    }
+    void InitAsDescriptorTable(UINT RangeCount, D3D12_SHADER_VISIBILITY Visibility = D3D12_SHADER_VISIBILITY_ALL);
+    void SetTableRange(UINT RangeIndex, D3D12_DESCRIPTOR_RANGE_TYPE Type, UINT Register, UINT Count, UINT Space = 0);
 };
 
 
@@ -95,47 +35,14 @@ class RootSignature
     std::wstring mName{};
 
 public:
-    RootSignature(const std::wstring& name, UINT size) : mName(name)
-    {
-        for (size_t i = 0; i < size; i++)
-        {
-            mParams.emplace_back();
-        }
-    }
+    RootSignature(const std::wstring& name, UINT size);
+    ~RootSignature();
 
-    ~RootSignature() {}
-
-    inline RootParameter* GetParam(size_t entryIndex)
-    {
-        if (IsIndexValid(entryIndex))
-        {
-            return &mParams[entryIndex];
-        }
-        return nullptr;
-    }
-
-    inline const RootParameter* GetParam(size_t entryIndex) const
-    {
-        if (IsIndexValid(entryIndex))
-        {
-            return &mParams[entryIndex];
-        }
-        return nullptr;
-    }
-
+    RootParameter* GetParam(size_t entryIndex);
+    const RootParameter* GetParam(size_t entryIndex) const;
     void Finalize(D3D12_ROOT_SIGNATURE_FLAGS Flags = D3D12_ROOT_SIGNATURE_FLAG_NONE);
-
     [[nodiscard]] inline ID3D12RootSignature* GetSignature() const { return mRootSignature.Get(); }
+    [[nodiscard]] bool IsIndexValid(size_t entryIndex) const noexcept;
 
-    [[nodiscard]] inline bool IsIndexValid(size_t entryIndex) const noexcept
-    {
-        assert(entryIndex < mParams.size());
-        if (entryIndex >= mParams.size())
-        {
-            LOG_ERROR("Index is out of range");
-            return false;
-        }
-        return true;
-    }
 };
 
